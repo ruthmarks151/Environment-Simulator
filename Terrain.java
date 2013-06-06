74a612e6e75c787658b1311b6292305033dd5e02
Christian W <christian.wen@gmail.com>
2013-06-06 14:01:15 -0400

    Protected int, alt initialize, sd => 25
    Not sure if we need protected data fields but did that.  Will need get
    methods.  Add alternate initialization with preset values you provide.
    Set min and max  of 0-100 for light and water. -46 to 42 degrees Celsius
    for the temp.
    
    I Also changed sd to 25.  Gives more room to work with.

--- Terrain.java
+++ Terrain.java
@@ -1,23 +1,58 @@
 import java.util.Random;
 class Terrain
 {
-  int light; //sd 20, mean 50, min 0, max 100
-  int water; //same as light
-  int temp; //
+  protected int light; //sd 25, mean 50, min 0, max 100
+  protected int water; //same as light
+  protected int temp; //
   
   public Terrain ()
   {
     Random r = new Random();
     do
     {
-    light = (int)Math.round(r.nextGaussian()*20)+50;
+    light = (int)Math.round(r.nextGaussian()*25)+50;
     }
     while (light > 100 && light < 0);
     do
     {
-    water = (int)Math.round(r.nextGaussian()*20)+50;
+    water = (int)Math.round(r.nextGaussian()*25)+50;
     }
     while (water > 100 && water < 0);
     
   }
+  
+  //Initialize the Environment with given constructors
+  public Terrain (int lite, int h20, int temperature)
+  {
+    light = lite;
+    if (light < 0)
+    {
+      light = 0;
+    }
+    else if (light > 100)
+    {
+      light = 100;
+    }
+    
+    water = h20;
+    if (water < 0)
+    {
+      water = 0;
+    }
+    else if (water > 100)
+    {
+      water = 100:
+    }
+    
+    temp = temperature;
+    if (temp < -46)
+    {
+      temp = -46;
+    }
+    else if (temp > 42)
+    {
+      temp = 42;
+    }
+  }
+  
 }
